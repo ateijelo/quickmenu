3d573adf6145bb86169e4d161e7adc36c39bb810
Andy Teijelo <ateijelo@gmail.com>
2014-10-12 15:02:07 -0400

    Missing files!

---
+++ quickmenu.cpp
@@ -0,0 +1,232 @@
+/*
+
+QuickMenu Copyright (C) 2014 Andy Teijelo <ateijelo@gmail.com>
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+1. Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the distribution.
+3. Neither the name of the University nor the names of its contributors
+   may be used to endorse or promote products derived from this software
+   without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+SUCH DAMAGE.
+
+*/
+
+#include <QtDebug>
+#include <QFile>
+#include <QAction>
+#include <QJsonDocument>
+#include <QJsonArray>
+#include <QApplication>
+#include <QMessageBox>
+#include <QProcess>
+#include <QFileInfo>
+#include <QDir>
+#include <QTimer>
+
+#include "quickmenu.h"
+
+void QuickMenu::show()
+{
+    icon.show();
+}
+
+QuickMenu::QuickMenu(QString jsonPath, QObject *parent) :
+    QObject(parent), jsonPath(jsonPath)
+{
+    loadRootMenu();
+    icon.setContextMenu(&rootMenu);
+
+    watcher.addPath(jsonPath);
+    connect(&watcher,SIGNAL(fileChanged(QString)),this,SLOT(fileChanged()));
+}
+
+QJsonDocument QuickMenu::readJsonFile()
+{
+    QJsonDocument rootDoc;
+
+    QFile f(jsonPath);
+    if (!f.open(QIODevice::ReadOnly))
+    {
+        rootMenu.addAction(QIcon::fromTheme("dialog-warning"), QString("Error opening %1: %2")
+                             .arg(jsonPath)
+                             .arg(f.errorString()));
+        return rootDoc;
+    }
+
+    QByteArray data;
+    QJsonParseError parseError;
+
+    data = f.readAll();
+    rootDoc = QJsonDocument::fromJson(data,&parseError);
+    if (rootDoc.isNull())
+    {
+        std::pair<int,int> p = position(data, parseError);
+        rootMenu.addAction(QIcon::fromTheme("dialog-warning"),QString("Parse Error: %1: %2:%3: %4")
+                                 .arg(jsonPath)
+                                 .arg(p.first)
+                                 .arg(p.second)
+                                 .arg(parseError.errorString()));
+    }
+    return rootDoc;
+}
+
+void QuickMenu::loadRootMenu()
+{
+    rootMenu.clear();
+    foreach (QMenu *m, subMenus)
+    {
+        m->deleteLater();
+    }
+    subMenus.clear();
+
+    auto rootDoc = readJsonFile();
+
+    if (!(rootDoc.isNull()))
+    {
+        auto rootObj = rootDoc.object();
+        addMenu(&rootMenu, rootObj);
+
+        if (rootMenu.icon().isNull())
+        {
+            icon.setIcon(QIcon(":/default.png"));
+        }
+        else
+        {
+            icon.setIcon(rootMenu.icon());
+        }
+    }
+    else
+    {
+        icon.setIcon(QIcon(":/default-warning.png"));
+    }
+
+    rootMenu.addAction("Quit",qApp,SLOT(quit()));
+}
+
+void QuickMenu::actionTriggered()
+{
+    QAction* a = (QAction *)sender();
+    QJsonObject entry = a->data().toJsonObject();
+    QString pwd = entry.value("pwd").toString();
+    QStringList args;
+    args << "-c";
+    args << entry.value("action").toString();
+    QProcess::startDetached("sh",args,pwd);
+}
+
+void QuickMenu::showError(const QString &title, const QString &msg)
+{
+    icon.showMessage(title, msg);
+}
+
+void QuickMenu::fileChanged()
+{
+    loadRootMenu();
+}
+
+void QuickMenu::newConnection()
+{
+    icon.contextMenu()->popup(icon.geometry().topLeft());
+}
+
+void QuickMenu::addMenu(QMenu *menu, const QJsonObject &obj)
+{
+    if (!obj.contains("menu")) return;
+
+    menu->setIcon(loadIcon(obj));
+
+    if (obj.contains("shortcut"))
+    {
+        menu->menuAction()->setShortcut(QKeySequence(obj.value("shortcut").toString()));
+    }
+
+    QJsonArray entries = obj.value("menu").toArray();
+    foreach (QJsonValue e, entries)
+    {
+        if (!e.isObject())
+            continue;
+        auto entry = e.toObject();
+        auto label = entry.value("label").toString();
+        if (entry.contains("menu"))
+        {
+            QMenu *subMenu = menu->addMenu(label);
+            subMenus.insert(subMenu);
+            addMenu(subMenu, entry);
+        }
+        else if (entry.contains("action"))
+        {
+            QAction *action = menu->addAction(loadIcon(entry),label,this,SLOT(actionTriggered()));
+            action->setData(QVariant(entry));
+        }
+    }
+}
+
+QIcon QuickMenu::loadIcon(const QJsonObject &obj)
+{
+    QFileInfo fi(jsonPath);
+    auto iconPath = fi.dir().absoluteFilePath(obj.value("icon").toString());
+    if (QFile::exists(iconPath))
+        return QIcon(iconPath);
+    return QIcon();
+}
+
+std::pair<int, int> QuickMenu::position(const QByteArray &data, const QJsonParseError &parseError)
+{
+    int line = 1;
+    int column = 1;
+    for (int i=0; i<parseError.offset; i++)
+    {
+        if (data.at(i) == '\n')
+        {
+            line += 1;
+            column = 0;
+        }
+        column++;
+    }
+    return {line,column};
+}
+
+void QuickMenu::listenOn(const QString &name)
+{
+    server = new QLocalServer(this);
+    int attempts = 2;
+    while (true)
+    {
+        attempts--;
+        server->listen(name);
+        if (server->isListening())
+        {
+            break;
+        }
+        else
+        {
+            QLocalServer::removeServer(name);
+        }
+        if (attempts <= 0)
+        {
+            qDebug() << QString("Failed to create local server '%1'").arg(name);
+            break;
+        }
+    }
+    connect(server,SIGNAL(newConnection()),this,SLOT(newConnection()));
+}
+
---
+++ quickmenu.h
@@ -0,0 +1,78 @@
+/*
+
+QuickMenu Copyright (C) 2014 Andy Teijelo <ateijelo@gmail.com>
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+1. Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the distribution.
+3. Neither the name of the University nor the names of its contributors
+   may be used to endorse or promote products derived from this software
+   without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+SUCH DAMAGE.
+
+*/
+
+#ifndef QUICKMENU_H
+#define QUICKMENU_H
+
+#include <QSet>
+#include <QIcon>
+#include <QMenu>
+#include <QObject>
+#include <QSystemTrayIcon>
+#include <QJsonObject>
+#include <QJsonParseError>
+#include <QFileSystemWatcher>
+#include <QLocalSocket>
+#include <QLocalServer>
+
+class QuickMenu : public QObject
+{
+    Q_OBJECT
+    public:
+        explicit QuickMenu(QString jsonPath, QObject *parent = 0);
+        void listenOn(const QString& name);
+        void show();
+
+    private slots:
+        void actionTriggered();
+        void showError(const QString& title, const QString& msg);
+        void fileChanged();
+        void newConnection();
+
+    private:
+        QJsonDocument readJsonFile();
+        void addMenu(QMenu *menu, const QJsonObject& obj);
+        void loadRootMenu();
+        QIcon loadIcon(const QJsonObject& obj);
+        std::pair<int, int> position(const QByteArray& data, const QJsonParseError& parseError);
+
+        QSystemTrayIcon icon;
+        QMenu rootMenu;
+        QString jsonPath;
+        QFileSystemWatcher watcher;
+        QSet<QMenu *> subMenus; // QMenu::clear doesn't delete submenus, I keep track of them here
+                                // to delete them myself on reload
+        QLocalServer *server;
+
+};
+
+#endif // QUICKMENU_H
